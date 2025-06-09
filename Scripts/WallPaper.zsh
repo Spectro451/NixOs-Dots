fb69bbe662a4d57a42adc3d2107b189ed1abdfb4
Spectro451 <bastyfarias@gmail.com>
2025-06-09 19:26:54 -0400

    solucion de errores

--- Scripts/WallPaper.zsh
+++ Scripts/WallPaper.zsh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
-#
-# Elige un wallpaper aleatorio
-wall=$(find ../WallPaper/ -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.gif" \) | shuf -n 1)
-
-# Inicia swww-daemon si no está corriendo
-pgrep -x swww-daemon > /dev/null || swww-daemon &
-
-# Espera un poco para que swww-daemon esté listo (evita errores de socket)
+wallpapers=(~/.config/hypr/WallPaper/*.{jpg,jpeg,png,gif})
+wall="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+pgrep -x swww-daemon >/dev/null || swww-daemon &
 sleep 1
-
-# Cambia el fondo
 swww img "$wall"
-
-# Copia el wallpaper actual a la ruta que usa Hyprlock
 cp "$wall" ~/.config/hypr/wallpaper.jpg
